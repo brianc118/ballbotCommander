bc0be137de51ddfd8e8b682d06a276772893a375
Brian Chen <brianchen118@gmail.com>
2016-02-03 19:57:38 +1000

    Bug fixes. UI change (added fps labels). Added autoconnect when disconnected. Added timestamp on console output. Increased main window size. Removed legend from plot. Removed commenting. Removed print() calls for debugging.

--- ballbotCommander.py
+++ ballbotCommander.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from functools import partial
 import serial
 from serial import Serial
 import serial.tools.list_ports
 import pyqtgraph as pg
+import numpy as np
 #from pyqtgraph.Qt import QtCore, QtGui
 import PyQt4.QtCore as QtCore
 from PyQt4.QtCore import Qt, QThread, SIGNAL, QTimer, QDateTime
 import PyQt4.QtGui as QtGui
 import os
 import time
+import datetime
 
 about = "ballbotCommander by Brian Chen"
 version = "0.1"
@@ -20,6 +23,24 @@ pg.mkQApp()
 path = os.path.dirname(os.path.abspath(__file__))
 uiFile = os.path.join(path, 'ballbotCommander.ui')
 WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType(uiFile)
+
+class fpsObj(object):
+	def __init__(self):
+		self.now = 0
+		self.dt = 0
+		self.lastTime = 0
+		self.fps = None
+
+	def update(self):
+		self.now = time.clock()
+		self.dt = self.now - self.lastTime
+		self.lastTime = self.now
+		if self.fps is None:
+			self.fps = 1.0/self.dt
+		else:
+			s = np.clip(self.dt*3., 0, 1)
+			self.fps = self.fps * (1-s) + (1.0/self.dt) * s
+		return self.fps
 
 class AboutDialog(QtGui.QDialog):
 	def __init__(self, parent = None):
@@ -52,6 +73,7 @@ class MainWindow(TemplateBaseClass):
 		self.names = []
 		self.nameCheckItems = []
 		self.receiveNewDataCnt = 0
+		self.receiveNewDataFpsObj = fpsObj()
 
 		# Remote control
 		self.speed = '$'
@@ -59,10 +81,16 @@ class MainWindow(TemplateBaseClass):
 		# Serial Thread
 		self.serThread = None
 
+		# Timer for restarting serial thread
+		self.portTimer = QTimer()
+		self.portConnectedTries = 0
+
+
 		# Graph
 		self.curves = []
 		self.curveStates = []
 		self.curveColors = []
+		self.plotFpsObj = fpsObj()
 
 		# Creater timer for plotting
 		self.plotTimer = QTimer()
@@ -84,17 +112,21 @@ class MainWindow(TemplateBaseClass):
 		self.ui.clearGraphButton.clicked.connect(self.clearPlot)
 		self.ui.enableRCCheckBox.toggled.connect(self.toggleRC)
 		self.ui.actionAbout.triggered.connect(self.about)
-		self.ui.MyPlotWidget.addLegend()
+		# self.ui.MyPlotWidget.addLegend()
 
 		# Graph listview
 		self.listViewModel = QtGui.QStandardItemModel()
 		self.ui.listView.setModel(self.listViewModel)
 
 		# Read arrow keys
-		self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Up), self), QtCore.SIGNAL('activated()'), self.keyUp)
-		self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Down), self), QtCore.SIGNAL('activated()'), self.keyDown)
-		self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Left), self), QtCore.SIGNAL('activated()'), self.keyLeft)
-		self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Right), self), QtCore.SIGNAL('activated()'), self.keyRight)
+		self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Up), self), 
+			QtCore.SIGNAL('activated()'), self.keyUp)
+		self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Down), self), 
+			QtCore.SIGNAL('activated()'), self.keyDown)
+		self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Left), self), 
+			QtCore.SIGNAL('activated()'), self.keyLeft)
+		self.connect(QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Right), self), 
+			QtCore.SIGNAL('activated()'), self.keyRight)
 
 		self.show()
 
@@ -152,12 +184,13 @@ class MainWindow(TemplateBaseClass):
 		self.receiveNewDataCnt = 0
 
 	def listPorts(self):
-		print(self.ui.menuPorts)
+		
 		self.ui.menuPorts.clear()
 		self.ui.menuPorts.addAction(self.actionRefresh)
 
 		ports = list(serial.tools.list_ports.comports())
 		ports.sort()
+		# print(ports)
 
 		for port in ports:
 			portName = port[0]
@@ -169,26 +202,74 @@ class MainWindow(TemplateBaseClass):
 		connectAction.triggered.connect(lambda: self.connectToPort(name))
 		menu.addAction(connectAction)
 
-	def connectToPort(self, portName):
-		self.plotTimer.stop()
+	def connectToPortPeriodic(self, p):
+		self.resetPlot()
+
+		self.portTimer.timeout.connect(partial(self.connectToPort, portName=p))
+		self.portConnectedTries = 0
+		self.portTimer.start(2000)
+
+	def resetPlot(self):
+		self.data = []
+		self.dataInitialised = False
+		self.dataColumns = 0
+		self.names = []
+		self.receiveNewDataCnt = 0
+
+		# if self.plotTimer.isActive():
+		# 	self.plotTimer.stop()
+
+		# clear plot
+		self.ui.MyPlotWidget.clear()		
 
 		# clear listView items
 		self.listViewModel.clear() 
 		self.nameCheckItems = []
 
+		# curves
+		self.curves = []
+		self.curveStates = []
+		self.curveColors = []
+
+	def connectToPort(self, portName):
+		retry = self.portTimer.isActive()
+		# make sure timer doesn't start multiple attempts at connecting at the same time
+		self.portTimer.stop()
+
+		self.resetPlot()
+
+		# try up to 5 times to reconnect
+		if self.portConnectedTries > 4:
+			self.portConnectedTries = 1
+			retry = False
+		else:
+			self.portConnectedTries = self.portConnectedTries + 1
+
+		# terminate existing serial thread if exists
 		if self.serThread is not None:
 			if self.serThread.isRunning():
-				self.procEnd.emit() # end serial thread
+				self.appendToConsole("Terminating existing serial thread\n")
+				self.procEnd.emit()
 				time.sleep(1)
 
-		self.appendToConsole("Creating new thread\n", QtGui.QColor('green'))
+		self.appendToConsole("Trying to connect to %s (try %d)\n" % 
+			(portName, self.portConnectedTries), QtGui.QColor('green'))
+
 		try:
 			self.serThread = SerialThread(portName, 115200)
 			self.appendToConsole("Connected to %s\n" % portName, QtGui.QColor('green'))
+
+			# this is the dodgy way to disconnect the timer
+			self.portTimer = None
+			self.portTimer = QTimer()
 		except:
 			self.appendToConsoleErr("Error connecting to %s\n" % portName)
 			self.procEnd.emit() # end serial thread
+			if retry:
+				self.appendToConsoleErr("Retrying in 2s\n")
+				self.portTimer.start(2000)
 			return
+
 		self.serThread.newDataSig.connect(self.receiveNewData)
 		self.serThread.newNamesSig.connect(self.receiveNewNames)
 		self.procSend.connect(self.serThread.send_text)
@@ -196,18 +277,12 @@ class MainWindow(TemplateBaseClass):
 		self.connect(self.serThread, SIGNAL("append_text(QString)"), self.ui.plainTextEdit.appendPlainText)
 		self.connect(self.serThread, SIGNAL("append_console(QString)"), self.appendToConsole)
 		self.connect(self.serThread, SIGNAL("append_console_err(QString)"), self.appendToConsoleErr)
+		self.connect(self.serThread, SIGNAL("serial_restart_mode(QString)"), self.connectToPortPeriodic)
 		self.serThread.start()
-		
-		# Create variables
-		self.data = []
-		self.dataInitialised = False
-		self.dataColumns = 0
-		self.names = []
-		self.receiveNewDataCnt = 0
 
 		time.sleep(0.5)
 
-		self.plotTimer.start(50)  # 20Hz timer
+		self.plotTimer.start(30)  # 20Hz timer
 
 	def receiveNewData(self, myNewData):	
 		for newDataRow in myNewData:
@@ -221,20 +296,25 @@ class MainWindow(TemplateBaseClass):
 		if self.dataInitialised is False:
 			self.dataInitialised = True
 
+		self.ui.statusBarLabelReceiveFps.setText(
+				"Receiving at %.0f fps" % self.receiveNewDataFpsObj.update())
+
 	def receiveNewNames(self, newNames):
-		print("Receiving new names")
+		# print("Receiving new names")
 		self.names = newNames
 		self.dataColumns = len(self.names)
 
-		for i in range(self.dataColumns):      
+		for i in range(self.dataColumns):	  
 			color = pg.intColor(i, self.dataColumns) 
-			self.curveColors.append(color)    
+			self.curveColors.append(color)	
 			item = QtGui.QStandardItem(self.names[i])
 			item.setBackground(color)
 			item.setCheckState(Qt.Checked)
 			item.setCheckable(True)
 			self.listViewModel.appendRow(item)
 			self.nameCheckItems.append(item)
+
+		# print("Received %d new names" % self.dataColumns)
 
 	def curveEnabled(self, name):
 		for i in range(self.dataColumns):
@@ -244,10 +324,7 @@ class MainWindow(TemplateBaseClass):
 
 	def plot(self):
 		if len(self.curves) <= 0 and self.dataInitialised:
-			rgn = pg.LinearRegionItem([self.data[0][0],self.data[0][0]+1000000])
-			self.ui.MyPlotWidget.addItem(rgn)
 			# initialise curves
-			print(self.dataColumns)
 			for i in range(self.dataColumns):
 				curve = pg.PlotCurveItem(pen=self.curveColors[i], name=self.names[i])
 				self.ui.MyPlotWidget.addItem(curve)
@@ -262,7 +339,6 @@ class MainWindow(TemplateBaseClass):
 					if self.curveEnabled(self.nameCheckItems[i].text()) is False:
 						self.ui.MyPlotWidget.addItem(self.curves[i])
 						self.curveStates[i] = True
-						print ("adding")
 
 					if (len(self.data[0]) == len(self.data[i+1])):
 						self.curves[i].setData(self.data[0][-10000:],self.data[i+1][-10000:])
@@ -271,7 +347,9 @@ class MainWindow(TemplateBaseClass):
 					if self.curveEnabled(self.nameCheckItems[i].text()):
 						self.ui.MyPlotWidget.removeItem(self.curves[i])
 						self.curveStates[i] = False
-						print ("removing")
+
+			
+			self.ui.statusBarLabelPlotFps.setText("Plotting at %.0f fps" % self.plotFpsObj.update())
 
 			if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
 				QtGui.QApplication.processEvents()
@@ -292,8 +370,11 @@ class MainWindow(TemplateBaseClass):
 
 	def appendToConsole(self, text, color = QtGui.QColor('red')):
 		self.ui.console.setTextColor(color)
+
+		current_time = datetime.datetime.now().time()
+
 		# cursor should always be at end of contents of QTextEdit
-		self.ui.console.insertPlainText("$ %s" % text)
+		self.ui.console.insertPlainText("%s $ %s" % (current_time.strftime("%H:%M:%S"), text))
 
 		c =  self.ui.console.textCursor();
 		c.movePosition(QtGui.QTextCursor.End);
@@ -388,39 +469,45 @@ class SerialThread(QThread):
 	newNamesSig = QtCore.pyqtSignal(object)
 	def __init__(self, port_, baud_, t_ = 1, delim_ = '\t', dirMsgSig_ = 'd'):
 		QThread.__init__(self)
+		self.portName = port_
 		self.enabled = True
+		self.connected = False
 		self.serParser = None
 		try:
 			self.serParser = SerialParser(port_, baud_, t = t_, delim = delim_, dirMsgSig = dirMsgSig_)
+			self.connected = True
 		except:
 			raise Exception("SerialThread_serialerror")
+			self.enabled = False
 	def __del__(self):
 		self.wait()
 	def run(self):
 		# self.start()
-		self.serParser.getNames()
-		self.newNamesSig.emit(self.serParser.names)
+		if self.connected:
+			self.serParser.getNames()
+			self.newNamesSig.emit(self.serParser.names)
 
-		while self.enabled:
-			try:
-				self.serParser.read()
-			except:
-				self.emit(SIGNAL('append_console_err(QString)'), "Error reading from serial\n")
-				self.enabled = False
-			if len(self.serParser.consoleBuff) > 0:
-				self.emit(SIGNAL('append_console(QString)'), self.serParser.consoleBuff[1:])
-				self.serParser.consoleBuff = ""
-			if len(self.serParser.printBuff) > 0:
-				self.emit(SIGNAL('append_text(QString)'), self.serParser.printBuff.rstrip())
-				self.serParser.printBuff = ""
-			if len(self.serParser.newData) > 0:
-				self.newDataSig.emit(self.serParser.newData)
-				self.serParser.newData = []
+			while self.enabled:
+				try:
+					self.serParser.read()
+				except:
+					self.emit(SIGNAL('append_console_err(QString)'), "Error reading from serial\n")
+					self.enabled = False
+				if len(self.serParser.consoleBuff) > 0:
+					self.emit(SIGNAL('append_console(QString)'), self.serParser.consoleBuff[1:])
+					self.serParser.consoleBuff = ""
+				if len(self.serParser.printBuff) > 0:
+					self.emit(SIGNAL('append_text(QString)'), self.serParser.printBuff.rstrip())
+					self.serParser.printBuff = ""
+				if len(self.serParser.newData) > 0:
+					self.newDataSig.emit(self.serParser.newData)
+					self.serParser.newData = []
 
-		self.emit(SIGNAL('append_console_err(QString)'), "Closing serial port\n")
-		self.serParser.ser.close()
-		self.serParser.ser = None
-		self.emit(SIGNAL('append_console_err(QString)'), "Closing serial thread\n")
+			self.emit(SIGNAL('append_console_err(QString)'), "Closing serial port\n")
+			self.emit(SIGNAL('serial_restart_mode(QString)'), self.portName)
+			self.serParser.ser.close()
+			self.serParser.ser = None
+			self.emit(SIGNAL('append_console_err(QString)'), "Closing serial thread\n")
 
 	@QtCore.pyqtSlot(str)
 	def send_text(self, text):
